12b8111dca9dedf1ca6da8001db1ada67bd71442
= <evebramley@me.com>
2019-08-08 14:45:05 +0100

    iffavourite true/false

--- src/components/BeerItem.vue
+++ src/components/BeerItem.vue
@@ -1,16 +1,35 @@
 <template lang="html">
   <div>
-    
-    <p>{{beer.name}}</p>
+    <p @click="handleClick">
+      {{beer.name}}
+      <span v-if="isfavourite">✅</span>
+    </p>
   </div>
 </template>
 
 <script>
+import {eventBus} from '../main';
+
 export default {
   name: 'beer-item',
-  props: [ 'beer' ]
+  props: [ 'beer' ],
+  data(){
+    return {
+      isfavourite: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.isfavourite = true;
+      eventBus.$emit('favouriteBeer', this.beer);
+    }
+  }
 }
+
 </script>
 
 <style lang="css" scoped>
+p {
+  cursor: pointer;
+}
 </style>
